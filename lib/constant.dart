8afb458ba2891882024e7e61cf9b6ea908ec6e51
Diwakar <gandhi252000@gmail.com>
2023-06-24 01:52:22 +0530

    created one to one video call

---
+++ lib/constant.dart
@@ -0,0 +1,6 @@
+class MyConst {
+  // add your appId and appSign from console
+  static const int appId = 2043151845;
+  static const String appSign =
+      "e378f4c88659fde05bd48555d6943570f0f9ee43ec673f244e5ec63bc36b1633";
+}
